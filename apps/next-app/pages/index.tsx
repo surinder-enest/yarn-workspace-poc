a276081707fbe78d64a95be0b1d329fe5012ed68
Surinder <surinder.enest@gmail.com>
2020-08-23 14:30:52 +0530

    minor cleanups

--- apps/next-app/pages/index.tsx
+++ apps/next-app/pages/index.tsx
@@ -1,18 +1,15 @@
 import { Title, Paragraph } from '@mindme/shared';
-import Link from 'next/link';
 import Layout from '../components/Layout';
+import { Component } from 'react';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-    <Title title="Hello This is title" />
-    <Paragraph description="Hello This is paragraph" />
-  </Layout>
-);
-
-export default IndexPage;
+class App extends Component {
+  render() {
+    return (
+      <Layout title="Home | Next.js + TypeScript Example">
+        <Title title="Hello This is title" />
+        <Paragraph description="Hello This is paragraph" />
+      </Layout>
+    );
+  }
+}
+export default App;
