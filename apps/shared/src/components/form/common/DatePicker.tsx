4b7936222a602d215b3f0b18e2b6a047778c6791
Lakhwinder_enest <lakhwinder.enest@gmail.com>
2020-09-04 10:07:56 +0530

    add commands

--- apps/shared/src/components/form/common/DatePicker.tsx
+++ apps/shared/src/components/form/common/DatePicker.tsx
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
 import { StyleModel } from '../../../models';
-import DatePicker from 'react-bootstrap-date-picker';
+import DatePicker from 'react-16-bootstrap-date-picker';
 
 interface Props {
-    className?: string;
-    styles?: StyleModel;
-    dateFormat?: string;
+  className?: string;
+  styles?: StyleModel;
+  dateFormat?: string;
 }
 
 export default class InputControl extends Component<Props> {
-    render() {
-        const { className, styles } = this.props;
-        return <DatePicker
-            className={className}
-            style={styles}
-        />
-    }
+  render() {
+    const { className, styles } = this.props;
+    return <DatePicker className={className} style={styles} />;
+  }
 }
